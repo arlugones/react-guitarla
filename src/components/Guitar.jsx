57c313ad1c5e5cf54b065b0ce2ab0a9f87199f28
Alain Rodriguez <arlugones01@gmail.com>
2024-04-05 16:06:37 -0500

    Refactor Guitar component to destructure props

--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -1,17 +1,18 @@
 import Button from './Button'
 
-function Guitar(props) {
-
+function Guitar({guitar, addToCart}) {
+    
+    const { id, name, image, description, price } = guitar
     return (
         <div className="col-md-6 col-lg-4 my-4 row align-items-center">
                 <div className="col-4">
-                    <img className="img-fluid" src={`/img/${props.image}.jpg`} alt="imagen guitarra" />
+                    <img className="img-fluid" src={`/img/${image}.jpg`} alt="imagen guitarra" />
                 </div>
                 <div className="col-8">
-                    <h3 className="text-black fs-4 fw-bold text-uppercase">{props.name}</h3>
-                    <p>{props.description}</p>
-                    <p className="fw-black text-primary fs-3">${props.price}</p>
-                    <Button />
+                    <h3 className="text-black fs-4 fw-bold text-uppercase">{name}</h3>
+                    <p>{description}</p>
+                    <p className="fw-black text-primary fs-3">${price}</p>
+                    <Button guitarDetails={guitar} updateCart={addToCart}/>
                 </div>
             </div>
     )

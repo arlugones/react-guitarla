8feaad1c853ae6152063012d7a3b7d0a893eca1f
Alain Rodriguez <arlugones01@gmail.com>
2024-04-05 10:06:44 -0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react'
+import Header from './components/Header'
+import Guitar from './components/Guitar'
+import { db } from './data/db'
+
+function App() {
+
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    setData(db)
+  }, [])
+
+  return (
+    <>
+    <Header />
+    <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
+
+        <div className="row mt-5">
+            {data.map((item) => (
+                <Guitar name={item.name} image={item.image} description={item.description} price={item.price}/>
+            ))}
+
+            
+        </div>
+    </main>
+
+
+    <footer className="bg-dark mt-5 py-5">
+        <div className="container-xl">
+            <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+        </div>
+    </footer>
+
+      
+    </>
+  )
+}
+
+export default App

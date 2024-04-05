1124c0f64a2c761f59b004f100ab9ab391dcc7ca
Alain Rodriguez <arlugones01@gmail.com>
2024-04-05 16:06:32 -0500

    Add onClick event to Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,15 @@
 
-function Button(){
+function Button({guitarDetails, updateCart}){
+
+    // const handleClick = (guitarDetails) => {
+    //     updateCart(guitarDetails)
+    // }
+
     return (
         <button 
                     type="button"
                     className="btn btn-dark w-100"
+                    onClick = {() => updateCart(guitarDetails)}
                 >Agregar al Carrito</button>
     )
 }
